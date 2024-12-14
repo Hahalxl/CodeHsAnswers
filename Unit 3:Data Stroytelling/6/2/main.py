2f90fb1ae80861e707180a3adf7a83d95c8b7a11
Hahalxl <136863536+Hahalxl@users.noreply.github.com>
2024-12-14 16:29:53 -0500

    Create main.py

---
+++ Unit 3:Data Stroytelling/6/2/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the CSV file
+df = pd.read_csv("data.csv")
+pd.set_option("display.max_columns", None)
+df.drop("quarter", inplace=True, axis=1)
+#C plus plus dataset
+cpp = df.loc[df.name == "C++", :]
+cpp.drop_duplicates(subset=['year'], keep="last", inplace=True)
+cpp.drop("name", inplace=True, axis=1)
+plt.hist(cpp['count'], bins=10)
+
+
+plt.show()
