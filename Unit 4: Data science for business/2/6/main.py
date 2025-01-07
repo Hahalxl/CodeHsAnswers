03086afeeabfdf588ca15277361a69b3cd680454
Hahalxl <136863536+Hahalxl@users.noreply.github.com>
2025-01-07 14:15:33 -0500

    Create main.py

---
+++ Unit 4: Data science for business/2/6/main.py
@@ -0,0 +1,13 @@
+# Click on the Assignment tab to see more info. -->
+# <-- Click on the files to see the different datasets.
+
+import pandas as pd
+
+#First dataset
+df = pd.read_csv("internet_users.csv")
+print(df.isnull().mean().round(4) * 100)
+
+#Second dataset
+df = pd.read_csv("cereal.csv")
+print("\n\n\n")
+print(df.isnull().mean().round(4) * 100)
